0fd50ff3b9069e156fb7cae8cc13ca7cf287450f
javyer12 <jh69362@gmail.com>
2026-09-21 19:37:29 -0600

    nueva rama

--- build/Qt_6_11_2_for_macOS_Debug/ui_mainwindow.h
+++ build/Qt_6_11_2_for_macOS_Debug/ui_mainwindow.h
@@ -40,33 +40,37 @@ public:
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName("MainWindow");
         MainWindow->resize(800, 600);
-        MainWindow->setStyleSheet(QString::fromUtf8("background-color:qlineargradient(spread:pad, x1:0, y1:0, x2:1, y2:0, stop:0 rgba(0, 0, 0, 255), stop:1 rgba(255, 255, 255, 255))"));
+        MainWindow->setStyleSheet(QString::fromUtf8("background-color:rgba(235, 235, 235, 140);"));
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
         textEdit = new QTextEdit(centralwidget);
         textEdit->setObjectName("textEdit");
-        textEdit->setGeometry(QRect(10, 50, 691, 41));
-        textEdit->setStyleSheet(QString::fromUtf8("background-color:rgb(168, 168, 168);\n"
-"color: black;"));
+        textEdit->setGeometry(QRect(0, 0, 811, 41));
+        textEdit->setStyleSheet(QString::fromUtf8("background-color:rgb(61, 85, 76);\n"
+"color: white;"));
+        textEdit->setReadOnly(true);
         txtListas = new QTextBrowser(centralwidget);
         txtListas->setObjectName("txtListas");
-        txtListas->setGeometry(QRect(220, 200, 181, 201));
+        txtListas->setGeometry(QRect(40, 200, 181, 201));
         txtListas->setStyleSheet(QString::fromUtf8("background-color:white;\n"
-"color:black;"));
+"color:black;\n"
+"shadow: 2px green;"));
         lcdNumber = new QLCDNumber(centralwidget);
         lcdNumber->setObjectName("lcdNumber");
-        lcdNumber->setGeometry(QRect(590, 12, 101, 21));
+        lcdNumber->setGeometry(QRect(600, 110, 101, 21));
         buttonMostrarListado = new QPushButton(centralwidget);
         buttonMostrarListado->setObjectName("buttonMostrarListado");
-        buttonMostrarListado->setGeometry(QRect(260, 430, 100, 32));
-        buttonMostrarListado->setStyleSheet(QString::fromUtf8("color:green;"));
+        buttonMostrarListado->setGeometry(QRect(70, 430, 100, 32));
+        buttonMostrarListado->setStyleSheet(QString::fromUtf8("background-color:rgb(61, 85, 76);\n"
+"color: white;"));
         inputTipoListado = new QComboBox(centralwidget);
         inputTipoListado->addItem(QString());
         inputTipoListado->addItem(QString());
         inputTipoListado->addItem(QString());
         inputTipoListado->setObjectName("inputTipoListado");
-        inputTipoListado->setGeometry(QRect(240, 160, 161, 32));
-        inputTipoListado->setStyleSheet(QString::fromUtf8("color:rgb(0, 249, 0);"));
+        inputTipoListado->setGeometry(QRect(50, 150, 161, 32));
+        inputTipoListado->setStyleSheet(QString::fromUtf8("color:rgb(0, 249, 0);\n"
+"background-color:rgb(61, 85, 76);"));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
@@ -91,7 +95,7 @@ public:
 "li.unchecked::marker { content: \"\\2610\"; }\n"
 "li.checked::marker { content: \"\\2612\"; }\n"
 "</style></head><body style=\" font-family:'.AppleSystemUIFont'; font-size:13pt; font-weight:400; font-style:normal;\">\n"
-"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:24pt;\"> Welcome Kathia</span></p></body></html>", nullptr));
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:24pt;\"> Welcome Kathia o Katia?</span></p></body></html>", nullptr));
         buttonMostrarListado->setText(QCoreApplication::translate("MainWindow", "Ver Listado", nullptr));
         inputTipoListado->setItemText(0, QCoreApplication::translate("MainWindow", "Clientes", nullptr));
         inputTipoListado->setItemText(1, QCoreApplication::translate("MainWindow", "Productos", nullptr));
